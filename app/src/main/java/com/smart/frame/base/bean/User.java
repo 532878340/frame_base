692ffd2b69d126c3d4189013661921306e96f935
532878340 <532878340@qq.com>
2018-05-03 14:48:23 +0800

    新增user相关

---
+++ app/src/main/java/com/smart/frame/base/bean/User.java
@@ -0,0 +1,10 @@
+package com.smart.frame.base.bean;
+
+/**
+ * $desc$
+ *
+ * @author Gjm
+ * @date 2018/5/3
+ */
+public class User {
+}
---
+++ app/src/main/java/com/smart/frame/model/UserManager.java
@@ -0,0 +1,10 @@
+package com.smart.frame.model;
+
+/**
+ * $desc$
+ *
+ * @author Gjm
+ * @date 2018/5/3
+ */
+public class UserManager {
+}
